98fbf85ce898d34913369c3f48c3f54e21e93c7f
kewablay <kojokewajunior123@gmail.com>
2024-08-28 11:06:17 +0000

    added logics for getting invoice detail data

--- src/app/pages/invoice-detail/invoice-detail.component.ts
+++ src/app/pages/invoice-detail/invoice-detail.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { selectInvoiceById } from '../../store/invoices/invoices-selectors/invoices.selectors';
+import { ActivatedRoute } from '@angular/router';
+import { Invoice } from '../../models/invoice.model';
+import { Observable, switchMap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-detail',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './invoice-detail.component.html',
-  styleUrl: './invoice-detail.component.sass'
+  styleUrl: './invoice-detail.component.sass',
 })
 export class InvoiceDetailComponent {
+  invoice$!: Observable<Invoice | undefined>;
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    this.invoice$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        const invoiceId = params.get('invoiceId');
+        return this.store.select(selectInvoiceById(invoiceId || ''));
+      })
+    );
+  }
 
+  ngOnInit() {
+    this.invoice$.subscribe((invoice) =>
+      console.log('invoice from detail', invoice)
+    );
+  }
 }
