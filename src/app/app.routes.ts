dec784d6bb8d242e2176f3f84ed84b23ad266295
kewablay <kojokewajunior123@gmail.com>
2024-08-28 11:03:14 +0000

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+// import { InvoiceDetailComponent } from './pages/invoice-detail/invoice-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'invoice-detail/:invoiceId',
+    loadComponent: () =>
+      import('./pages/invoice-detail/invoice-detail.component').then(
+        (m) => m.InvoiceDetailComponent
+      ),
+  },
+];
