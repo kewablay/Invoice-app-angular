754e955f7070c2cb1ab53278bffc3caa59ee43f0
kewablay <kojokewajunior123@gmail.com>
2024-08-28 11:04:55 +0000

    added routes and removed initial load of invoices

--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -4,25 +4,21 @@ import { AppState } from '../../store/app.state';
 import { Observable } from 'rxjs';
 import { Invoice } from '../../models/invoice.model';
 import { selectFilteredInvoices } from '../../store/invoices/invoices-selectors/invoices.selectors';
-import { loadInvoices } from '../../store/invoices/invoices-actions/invoices.actions';
 import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-list',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.sass',
 })
 export class InvoiceListComponent {
   invoices$!: Observable<Invoice[]>;
+  singleInvoice$!: Observable<Invoice | undefined>;
 
   constructor(private store: Store<AppState>) {
-    this.invoices$ = store.select(selectFilteredInvoices);
-    this.invoices$.subscribe((invoices) => console.log(invoices));
-  }
-
-  ngOnInit() {
-    this.store.dispatch(loadInvoices());
+    this.invoices$ = this.store.select(selectFilteredInvoices);
   }
 }
