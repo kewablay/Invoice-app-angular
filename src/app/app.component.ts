36b91a27979c239d26226046941eb260834879c7
kewablay <kojokewajunior123@gmail.com>
2024-08-28 11:03:05 +0000

    added initial load of the invoices in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InvoiceListComponent } from "./components/invoice-list/invoice-list.component";
+import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
+import { loadInvoices } from './store/invoices/invoices-actions/invoices.actions';
+import { AppState } from './store/app.state';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, InvoiceListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
+  constructor(private store: Store<AppState>) {
+    // load data
+    this.store.dispatch(loadInvoices());
+  }
 }
