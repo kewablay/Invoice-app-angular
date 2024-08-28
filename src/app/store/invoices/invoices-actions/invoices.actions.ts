17ab859e828bdbb31a305ed00c4f6395bd5a079b
kewablay <kojokewajunior123@gmail.com>
2024-08-28 13:20:04 +0000

    added addInvoice, updateInvoice, deleteInvoice and updateFilters actions

--- src/app/store/invoices/invoices-actions/invoices.actions.ts
+++ src/app/store/invoices/invoices-actions/invoices.actions.ts
@@ -10,3 +10,21 @@ export const loadInVoicesError = createAction(
   '[Invoices ] Load Invoices Error',
   props<{ error: string }>()
 );
+
+export const addInvoice = createAction(
+  '[Invoices] Add Invoice',
+  props<{ invoice: Invoice }>()
+);
+export const updateInvoice = createAction(
+  '[Invoices] Update Invoice',
+  props<{ invoice: Partial<Invoice> & { id: string } }>()
+);
+export const deleteInvoice = createAction(
+  '[Invoices] Delete Invoice',
+  props<{ id: string }>()
+);
+
+export const updateFilters = createAction(
+  '[Invoices] Update Filters',
+  props<{ filters: { paid: boolean; pending: boolean; draft: boolean } }>()
+);
