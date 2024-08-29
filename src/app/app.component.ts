fdc75ed514e7f869da47c68f22d1c3dcb124d957
kewablay <kojokewajunior123@gmail.com>
2024-08-29 17:36:17 +0000

    added themes to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,34 @@ import { InvoiceListComponent } from './components/invoice-list/invoice-list.com
 import { loadInvoices } from './store/invoices/invoices-actions/invoices.actions';
 import { AppState } from './store/app.state';
 import { Store } from '@ngrx/store';
+import { loadTheme } from './store/theme/theme.actions';
+import { Observable } from 'rxjs';
+import { selectCurrentTheme } from './store/theme/theme.selectors';
+import { AsyncPipe } from '@angular/common';
+import { MainPanelComponent } from "./components/main-panel/main-panel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InvoiceListComponent],
+  imports: [RouterOutlet, InvoiceListComponent, AsyncPipe, MainPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
 export class AppComponent {
+  currentTheme$: Observable<string>;
   constructor(private store: Store<AppState>) {
     // load data
     this.store.dispatch(loadInvoices());
+    this.currentTheme$ = this.store.select(selectCurrentTheme);
+
+    // load theme
+    this.store.dispatch(loadTheme());
+
+    // set theme
+    this.currentTheme$.subscribe((theme) => {
+      document.documentElement.setAttribute('data-theme', theme);
+    });
   }
+
+  
 }
