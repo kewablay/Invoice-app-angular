9d34980e461e339da692d04c451edb229fa40dc5
kewablay <kojokewajunior123@gmail.com>
2024-08-29 22:15:17 +0000

    Added home header and invoice list to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InvoiceListComponent } from "../../components/invoice-list/invoice-list.component";
+import { HomeHeaderComponent } from "../../components/home-header/home-header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InvoiceListComponent],
+  imports: [InvoiceListComponent, HomeHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
