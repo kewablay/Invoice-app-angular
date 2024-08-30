22851aed0fbd995029eb9abadf970bc5a037092e
kewablay <kojokewajunior123@gmail.com>
2024-08-30 11:23:05 +0000

    added invoice detail content component

---
+++ src/app/components/invoice-detail-content/invoice-detail-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-detail-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-detail-content.component.html',
+  styleUrl: './invoice-detail-content.component.sass'
+})
+export class InvoiceDetailContentComponent {
+
+}
