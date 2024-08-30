366c4706b1488cc7f2a66fb58dde9c39f62f7c6c
kewablay <kojokewajunior123@gmail.com>
2024-08-30 10:02:47 +0000

    added logic to keep the checked filters whenever we initilize the header

--- src/app/components/home-header/home-header.component.ts
+++ src/app/components/home-header/home-header.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { Observable } from 'rxjs';
 import { updateFilters } from '../../store/invoices/invoices-actions/invoices.actions';
+import { selectFilters } from '../../store/invoices/invoices-selectors/invoices.selectors';
+import { Filters } from '../../store/invoices/invoice-state/invoice.state';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home-header',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './home-header.component.html',
   styleUrl: './home-header.component.sass',
 })
 export class HomeHeaderComponent {
   // invoice$: Observable
   total: number = 7
-  constructor(private store: Store<AppState>) {}
+  filters$: Observable<Filters>
+  constructor(private store: Store<AppState>) {
+    this.filters$ = this.store.select(selectFilters)
+  }
 
   updateFilter(filterType: string, event: Event) {
     const checked = (event.target as HTMLInputElement).checked;
     this.store.dispatch(updateFilters({ filterType, filterValue: checked }));
   }
+
 }
