892600f5f6a73c0bc85af169e89f25a9b44cf1b7
kewablay <kojokewajunior123@gmail.com>
2024-08-30 09:50:14 +0000

    added currency pipe and date pipe

--- src/app/components/invoice-card/invoice-card.component.ts
+++ src/app/components/invoice-card/invoice-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Invoice } from '../../models/invoice.model';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './invoice-card.component.html',
   styleUrl: './invoice-card.component.sass',
 })
