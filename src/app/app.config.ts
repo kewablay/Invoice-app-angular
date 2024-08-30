83b30abae021a658310cf4b4f9f3f8264439bfb5
kewablay <kojokewajunior123@gmail.com>
2024-08-30 22:11:19 +0000

    added prime  ng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,11 +9,13 @@ import { InvoicesEffects } from './store/invoices/invoices-effects/invoices.effe
 import { invoiceReducer } from './store/invoices/invoice-reducers/invoices.reducers';
 import { themeEffect } from './store/theme/theme.effects';
 import { themeReducer } from './store/theme/theme.reducers';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideEffects(InvoicesEffects),
+    importProvidersFrom(BrowserAnimationsModule),
     provideStore(),
     provideHttpClient(),
     provideEffects(themeEffect),
