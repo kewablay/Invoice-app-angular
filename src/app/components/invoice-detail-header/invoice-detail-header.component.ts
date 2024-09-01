eafcfe6defbe9385bf06df5315f9e73934398554
kewablay <kojokewajunior123@gmail.com>
2024-09-01 07:27:20 +0000

    added toast for mark as paid and mark as pending

--- src/app/components/invoice-detail-header/invoice-detail-header.component.ts
+++ src/app/components/invoice-detail-header/invoice-detail-header.component.ts
@@ -8,35 +8,34 @@ import {
   deleteInvoice,
   updateInvoice,
 } from '../../store/invoices/invoices-actions/invoices.actions';
-import { InvoiceFormComponent } from "../invoice-form/invoice-form.component";
+import { InvoiceFormComponent } from '../invoice-form/invoice-form.component';
 import { SidebarModule } from 'primeng/sidebar';
 import { ModalService } from '../../services/modalService/modal.service';
-import { Subscription } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-invoice-detail-header',
   standalone: true,
-  imports: [StatusComponent, InvoiceFormComponent, SidebarModule],
+  imports: [StatusComponent, InvoiceFormComponent, SidebarModule, ToastModule],
   templateUrl: './invoice-detail-header.component.html',
   styleUrl: './invoice-detail-header.component.sass',
+  providers: [MessageService],
 })
 export class InvoiceDetailHeaderComponent {
   @Input() invoice!: Invoice;
-  isEditModalOpen!: boolean; 
-  private modalSubscription: Subscription
+  isEditModalOpen!: boolean;
 
   constructor(
     private router: Router,
     private store: Store<AppState>,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private messageService: MessageService
   ) {
-    this.modalSubscription = this.modalService.isModalOpen('editInvoice')
-      .subscribe(isOpen => {
-        this.isEditModalOpen = isOpen;
-      });
+    this.modalService.isModalOpen('editInvoice').subscribe((isOpen) => {
+      this.isEditModalOpen = isOpen;
+    });
   }
-
-  
 
   deleteInvoice = (id: string) => {
     this.store.dispatch(deleteInvoice({ id }));
@@ -45,14 +44,35 @@ export class InvoiceDetailHeaderComponent {
 
   markAsPaid = (id: string) => {
     this.store.dispatch(updateInvoice({ invoice: { id, status: 'paid' } }));
+    this.showToast({
+      type: 'success',
+      message: 'Invoice marked as paid',
+      title: 'Status Updated',
+    });
   };
   markAsPending = (id: string) => {
     this.store.dispatch(updateInvoice({ invoice: { id, status: 'pending' } }));
+    this.showToast({
+      type: 'success',
+      message: 'Invoice marked as Pending',
+      title: 'Status Updated',
+    });
   };
 
   openEditModal = () => {
     this.modalService.openModal('editInvoice');
   };
+  closeEditModal = () => {
+    this.modalService.closeModal('editInvoice');
+  };
+
+  showToast(event: { type: string; message: string; title: string }) {
+    this.messageService.add({
+      severity: event.type,
+      summary: event.title,
+      detail: event.message,
+    });
+  }
 
   onSidebarVisibleChange(isVisible: boolean) {
     if (isVisible) {
